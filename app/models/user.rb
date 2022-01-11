4621d2d744431c4e9a3100c7053a5e25efcb6e21
shinya_works <shiinya.works.w@gmail.com>
2022-01-11 20:10:20 +0900

    Userモデル作成

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  before_save {self.email = email.downcase}
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 100 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+  validates :dapartment, lenght: { in: 2..30 }, allow blank: true
+  has_secure_password
+  validates :pasword, presence: true, length: { minimum: 6 }, allow_nil: true
+end
