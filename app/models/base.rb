0df37e75a359afc19b5934918b8e42f180f6f32d
shinya_works <shinya.works.w@gmail.com>
2022-01-26 15:55:38 +0900

    Basesのコントローラ作成

--- app/models/base.rb
+++ app/models/base.rb
@@ -1,2 +1,28 @@
 class Base < ApplicationRecord
+  def index
+
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+  
+  def update
+    if @object.update_attributes(params[:object])
+    flash[:success] = "拠点情報の更新に成功しました"
+      redirect_to bases_url
+    else
+      flash[:danger] = "拠点情報の更新に失敗しました"
+      render 'edit'
+    end
+  end
+  
+  
 end
