1c59f0d2275a5ead2d342d2532beab916edde120
shinya_works <shinya.works.w@gmail.com>
2022-01-27 09:18:30 +0900

    一時保存

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -3,4 +3,36 @@ class BaseController < ApplicationController
   def index
     @bases = Base.all
   end
+
+  def new
+    @base = Bases.new
+  end
+
+  def def create
+    if @base.save
+      flash[:success] = "拠点情報の作成に成功しました"
+      redirect_to bases_path
+    else
+      flash[:danger] = "拠点情報の作成に失敗しました"
+      render 'new'
+    end
+  end
+  
+
+  def edit
+  end
+
+  def update
+    if @base.update_attributes(base_params])
+      flash[:success] = "拠点情報の更新に成功しました"
+      redirect_to @object
+    else
+      flash[:danger] = "拠点情報の更新に失敗しました"
+      render 'edit'
+    end
+  end
+
+  def delete
+  end
+  
 end
