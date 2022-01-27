f793893bfc8b85e9057204fa9ae1e477a5bbec9b
shinya_works <shinya.works.w@gmail.com>
2022-01-27 21:25:35 +0900

    カラム追加認証日、認証㊞完了

--- db/migrate/20220120025709_add_overwork_colum_to_attendances.rb
+++ db/migrate/20220120025709_add_overwork_colum_to_attendances.rb
@@ -3,5 +3,7 @@ class AddOverworkColumToAttendances < ActiveRecord::Migration[5.1]
     add_column :attendances, :expected_end_time, :time
     add_column :attendances, :overtime, :time
     add_column :attendances, :business_processing_details, :string
+    add_column :attendances, :authentication_user, :string
+    add_column :attendances, :authentication_day, :time
   end
 end
